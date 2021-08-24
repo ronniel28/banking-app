140fb6256bc938508cf4c3f9874b55a67756efb4
Ronniel de Ramos <ronnielderamos@gmail.com>
2021-08-24 21:09:54 +0800

    modified log in page

--- src/components/NewLogIn.jsx
+++ src/components/NewLogIn.jsx
@@ -34,7 +34,7 @@ export default function NewlogIn (props){
       <div className="flex justify-center self-center  z-10">
         <div className="p-12 bg-white mx-auto rounded-2xl w-100 ">
             <div className="mb-4">
-              <h3 className="font-semibold text-2xl text-gray-800">Sign In </h3>
+              <h3 className="font-semibold text-2xl text-gray-800">Log In </h3>
               <p className="text-gray-500">Please log in to your account.</p>
             </div>
             <div className="space-y-5">
